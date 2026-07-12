543a899205a7da890501b9e21dd4801761e3540a
pberset <pberset@42lausanne.ch>
2026-07-12 12:05:14 +0200

    hello world

---
+++ go/ohayousekai/helloworld.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	fmt.Println("The current time is", time.Now())
+}
+
+// go run file.go || go build file.go
