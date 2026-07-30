761edb71738b83a871891195f3a2800b545bd1c2
pberset <pberset@42lausanne.ch>
2026-07-30 14:56:46 +0200

    types and interfaces

--- typescript/src/greet.ts
+++ typescript/src/greet.ts
@@ -1,5 +1,63 @@
-function greet(person: string, date: Date) {
-  console.log(`Hello ${person}, today is ${date.toDateString()!}`)
+interface Point {
+  x: number;
+  y: number;
+  z?: number;
 }
 
-greet("Github", new Date());
+interface Greeting {
+  name?: string;
+  date?: Date;
+}
+
+type troolean = true | false | "maybe";
+type userInputSanitized = string;
+
+function greet(greet: Greeting): void {
+  console.log(`Hello ${greet.name}, today is ${greet.date?.toDateString()!}`)
+}
+
+function print_coord(coord: Point): void {
+  if (coord.z != undefined)
+    console.log(`x = ${coord.x}, y = ${coord.y}, z = ${coord.z}`);
+  else
+    console.log(`x = ${coord.x}, y = ${coord.y}`);
+}
+
+//Although this is not necessary, I'll use a "main" scope in order to keep consistency with basically all compiled languages
+
+//main
+{
+  const greeting: Greeting = {
+    name: "Github",
+    date: new Date()
+  }
+
+  greet(greeting);
+
+  const coord: Point = {
+    x: 42,
+    y: 4.2,
+    z: 42.42
+  };
+
+  print_coord(coord);
+
+  let greetAgain: Greeting = {};
+
+  greet(greetAgain);
+
+  greetAgain.name = "Forgetfull programer";
+  greetAgain.date = new Date();
+
+  greet(greetAgain);
+
+  let coord_no_z: Point = {
+    x: 0,
+    y: 0
+  };
+
+  coord_no_z.x = 23;
+  coord_no_z.y = 2.3;
+
+  print_coord(coord_no_z);
+}
